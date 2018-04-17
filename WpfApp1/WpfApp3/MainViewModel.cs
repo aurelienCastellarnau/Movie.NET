940a5fcbeb79f7ae26da8acc0d2e5f65684b0929
Jean Billaud <billaudjean@gmail.com>
2018-04-17 15:52:47 -0700

    fin mvvm

---
+++ WpfApp1/WpfApp3/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace WpfApp3
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            Name = "Hello MVVM";
+            MyCommande = new RelayCommand(MyCommandeExecute, MyCommandeCanExecute);
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set 
+            {
+                _name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand MyCommande { get; }
+
+        void MyCommandeExecute()
+        {
+            Name = "Hello click!";
+        }
+
+        bool MyCommandeCanExecute()
+        {
+            return Name != "Hello click!";
+        }
+    }
+}
