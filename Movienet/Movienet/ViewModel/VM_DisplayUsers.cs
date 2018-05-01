1612fbdcce6a5ab9faa5c31ca60c758a8aa82a01
aurelienCastellarnau <castelllarnau.a@gmail.com>
2018-05-01 20:38:49 +0200

    Feature(userDetail): Possibilité de sélectionner un utilisateur et de le display a coté de la liste. problèmes: la vue UserDetail dépend du VM display_users Comment permettre d'ouvrir une nouvelle vue/VueModel avec le user en paramètre?

--- Movienet/Movienet/ViewModel/VM_DisplayUsers.cs
+++ Movienet/Movienet/ViewModel/VM_DisplayUsers.cs
@@ -4,7 +4,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using GalaSoft.MvvmLight;
+using ModelMovieNet;
 using ModelMovieNet.Factory;
 using ModelMovieNet.Interface;
 
@@ -12,20 +14,18 @@ namespace Movienet
 {
     public class VM_DisplayUsers: VM_User
     {
-        private List<ModelMovieNet.User> _users;
-        public ObservableCollection<string> UserList { get; set; } = new ObservableCollection<string>();
+        private ObservableCollection<User> _users { get; set; }
+        private User _selectItem;
+        private string _info;
+        private Page _detail;
 
         public VM_DisplayUsers()
         {
             Info = "Informations: ";
+            Detail = new Page();
             try
             {
-
-                Users = uDao.getAllUsers();
-                foreach (var user in Users)
-                {
-                    this.UserList.Add(user.Id + " - " + user.Firstname + " " + user.Lastname);
-                }
+                Users = new ObservableCollection<User>(uDao.getAllUsers());
             } catch (Exception e)
             {
                 Info = Info + e.Message;
@@ -33,20 +33,44 @@ namespace Movienet
             }
         }
 
-        public List<ModelMovieNet.User> Users
+        public ObservableCollection<User> Users
         {
             get { return _users; }
             set {
                 _users = value;
-                RaisePropertyChanged();
+                RaisePropertyChanged("Users");
             }
         }
 
-        private string _info;
-        public string Info
+        new public string Info
         {
             get { return _info; }
-            set { _info = value; }
+            set {
+                _info = value;
+                RaisePropertyChanged("Info");
+            }
         }
+
+        public User SelectItem
+        {
+            get { return _selectItem; }
+            set {
+                Console.WriteLine("set select item" + value.Firstname);
+                _selectItem = value;
+                Detail = new UserDetail();
+                Info = "Item selected";
+                RaisePropertyChanged("SelectedItem");
+            }
+        }
+
+        public Page Detail
+        {
+            get { return _detail; }
+            set {
+                _detail = value;
+                RaisePropertyChanged("Detail");
+            }
+        }
+
     }
 }
