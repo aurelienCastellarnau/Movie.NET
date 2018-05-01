27282f354b04a75f29faa1de4c8e0c32aea2076f
aurelienCastellarnau <castelllarnau.a@gmail.com>
2018-05-01 17:47:13 +0200

    feature(refactor_VM_User): regroupement des VM dans un folder, regroupement des vues dans un folder. Regroupement de add, update et delete dans le meme VM, display_users ailleurs.

---
+++ Movienet/Movienet/ViewModel/VM_DisplayUsers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using ModelMovieNet.Factory;
+using ModelMovieNet.Interface;
+
+namespace Movienet
+{
+    public class VM_DisplayUsers: VM_User
+    {
+        private List<ModelMovieNet.User> _users;
+        public ObservableCollection<string> UserList { get; set; } = new ObservableCollection<string>();
+
+        public VM_DisplayUsers()
+        {
+            Info = "Informations: ";
+            try
+            {
+
+                Users = uDao.getAllUsers();
+                foreach (var user in Users)
+                {
+                    this.UserList.Add(user.Id + " - " + user.Firstname + " " + user.Lastname);
+                }
+            } catch (Exception e)
+            {
+                Info = Info + e.Message;
+                Console.WriteLine("PERSO EXCEPTION: " + e.ToString());
+            }
+        }
+
+        public List<ModelMovieNet.User> Users
+        {
+            get { return _users; }
+            set {
+                _users = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string _info;
+        public string Info
+        {
+            get { return _info; }
+            set { _info = value; }
+        }
+    }
+}
---
+++ Movienet/Movienet/ViewModel/VM_User.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ModelMovieNet;
+using ModelMovieNet.Factory;
+using ModelMovieNet.Interface;
+
+namespace Movienet
+{
+    public class VM_User: ViewModelBase
+    {
+        protected static IServiceFacade Services { get; } = ServiceFacadeFactory.GetServiceFacade();
+        protected static IUserDao uDao { get; } = Services.GetUserDao();
+        public RelayCommand Add { get; set; } = null;
+        public RelayCommand Update { get; set; } = null;
+        public RelayCommand Delete { get; set; } = null;
+
+        private User _user;
+        private string _info;
+
+        public VM_User()
+        {
+            User = new User();
+            Add = new RelayCommand(AddUser, canAdd);
+            _info = "Informations: ";
+        }
+
+        public User User
+        {
+            get { return _user; }
+            set
+            {
+                Console.WriteLine("NewUser setter called");
+                _user = value;
+                if (Add != null)
+                    Add.RaiseCanExecuteChanged();
+                if (Update != null)
+                    Update.RaiseCanExecuteChanged();
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Firstname
+        {
+            get { return User.Firstname; }
+            set {
+                User.Firstname = value;
+                if (Add != null)
+                    Add.RaiseCanExecuteChanged();
+                if (Update != null)
+                    Update.RaiseCanExecuteChanged();
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Lastname
+        {
+            get { return User.Lastname; }
+            set
+            {
+                User.Lastname = value;
+                if (Add != null)
+                    Add.RaiseCanExecuteChanged();
+                if (Update != null)
+                    Update.RaiseCanExecuteChanged();
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Login
+        {
+            get { return User.Login; }
+            set
+            {
+                User.Login = value;
+                if (Add != null)
+                    Add.RaiseCanExecuteChanged();
+                if (Update != null)
+                    Update.RaiseCanExecuteChanged();
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get { return User.Password; }
+            set
+            {
+                User.Password = value;
+                if (Add != null)
+                    Add.RaiseCanExecuteChanged();
+                if (Update != null)
+                    Update.RaiseCanExecuteChanged();
+                RaisePropertyChanged();
+            }
+        }
+
+        protected Boolean isValidUser()
+        {
+            Console.WriteLine("isValidUser, BEGIN");
+            Boolean check = false;
+            if (User != null)
+            {
+                /*
+                 * Check property emptiness and size 
+                */
+                Console.WriteLine("isValidUser, newUsernotNULL");
+                check = (User.Firstname != null && User.Firstname.Length != 0 &&
+                    User.Lastname != null && User.Lastname.Length != 0 &&
+                    User.Login != null && User.Login.Length != 0 &&
+                    User.Password != null && User.Password.Length != 0 && User.Password.Length > 4);
+            }
+            Console.WriteLine("isValidUser return " + (check ? "true" : "false"));
+            return check;
+        }
+        
+        public string Info
+        {
+            get { return _info; }
+            set
+            {
+                string _info = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        void AddUser()
+        {
+            Console.WriteLine("In Add User from VM_AddUser");
+            String infoAdd = "Adding a user";
+            if (!isValidUser())
+            {
+                infoAdd = "You must fullfill all fields and password must be at least 4 characters";
+            }
+            else
+            {
+                User checkUser = new User();
+                try
+                {
+                    uDao.getAllUsers();
+                    checkUser = uDao.CreateUser(User);
+                    infoAdd = "Adduser passed but we dont know the id";
+                }
+                catch (Exception e)
+                {
+                    infoAdd = "Add user failed: " + e;
+                    Console.WriteLine("Exception PERSONNALISEE: " + e);
+                }
+                if (checkUser.Id > 0)
+                {
+                    infoAdd = "Everything happens well";
+                }
+            }
+            if (infoAdd != null)
+                Info = " Adding result: " + infoAdd;
+            Console.WriteLine("Info : " + infoAdd);
+        }
+
+        Boolean canAdd()
+        {
+            return isValidUser();
+        }
+    }
+}
